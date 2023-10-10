2bb864c177c553b56c85e8a851b70669a2258131
Ibraheem-hussain <146854639+Ibraheem-hussain@users.noreply.github.com>
2023-10-10 16:04:25 +0000

    fist c session

---
+++ week2_c_bootcamp/fourth.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main () {
+    int width;
+    int height;
+    printf("insert a width: ");
+    scanf("%d",);
+    printf("insert a height: ");
+    scanf("%d", &height);
+    int area = width * height;
+    printf("area = %d\n", area);
+    return 0;
+}
